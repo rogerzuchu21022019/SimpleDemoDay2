40815900c290dda294e82a25b13efdf73a7b27d9
rogerzuchu21022019 <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-05-09 15:36:30 +0700

    Initial Commit

---
+++ SimpleDemoDay2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleDemoDay2
+//
+//  Created by Vu Thanh Nam on 09/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
